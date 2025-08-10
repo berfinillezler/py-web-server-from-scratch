46818b0fc0aa33fea80de744bc7567a5cb614cbf
Berfin Illezler <bi53@kent.ac.uk>
2025-08-10 19:49:57 +0100

    Remove old nested web-server folder

--- web-server/static/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('JS loaded successfully!');
-  const h1 = document.querySelector('h1');
-  h1.textContent += ' (with JS!)';
-});
-
